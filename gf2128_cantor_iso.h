cfb42abf65f7db8c8739ccf6823d8a1a726e87a0
Ming-Shing Chen <mschen@crypto.tw>
2017-09-05 16:03:05 +0800

    fix typo.

--- gf2128_cantor_iso.h
+++ gf2128_cantor_iso.h
@@ -4,7 +4,7 @@ Copyright (C) 2017 Ming-Shing Chen
 This file is part of BitPolyMul.
 
 BitPolyMul is free software: you can redistribute it and/or modify
-it under the terms of the Lesser GNU General Public License as published by
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
